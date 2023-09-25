1f76d0fdff4aaadb958d71b260b976c40ab39699
Bryan Jonker <jonker@illinois.edu>
2023-09-25 14:51:25 -0500

    Adding option to delete test user

---
+++ Controllers/DeleteTestUserController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TqiiLanguageTest.BusinessLogic;
+using TqiiLanguageTest.Data;
+using TqiiLanguageTest.Models;
+
+namespace TqiiLanguageTest.Controllers {
+
+    [Route("deletetestuser")]
+    public class DeleteTestUserController : Controller {
+        private readonly LanguageDbContext _context;
+        private readonly PermissionsHandler _permissions;
+
+        public DeleteTestUserController(PermissionsHandler permissions, LanguageDbContext context) {
+            _permissions = permissions;
+            _context = context;
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Index(int id) {
+            if (!_permissions.IsAdmin(User.Identity?.Name ?? "")) {
+                return Unauthorized();
+            }
+            var testUser = _context?.TestUsers?.SingleOrDefault(tu => tu.Id == id);
+            if (testUser == null) {
+                return NotFound();
+            }
+            var answers = _context?.Answers?.Where(a => a.TestUserId == id).ToList() ?? new List<Answer>();
+            _context?.RemoveRange(answers);
+            _context?.Remove(testUser);
+            _context?.SaveChanges();
+            return Ok();
+        }
+    }
+}
