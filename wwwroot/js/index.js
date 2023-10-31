a42ad44944f591399477c2436bdc0ba3b8cb9dd8
Bryan Jonker <jonker@illinois.edu>
2023-10-31 12:28:29 -0500

    Emergency fix for listing multiple pages

---
+++ wwwroot/js/index.js
@@ -0,0 +1,13 @@
+﻿window.addEventListener("DOMContentLoaded", (event) => {
+    document.getElementById('start').addEventListener('click', event => {
+        event.target.style.display = 'none';
+        document.getElementById('hidden-continue').style.display = '';
+        window.open(event.target.getAttribute('data-href'), '_blank', 'popup=true');
+        return false;
+    });
+
+    document.getElementById('refresh').addEventListener('click', event => {
+        location.reload();
+        return false;
+    });
+});
